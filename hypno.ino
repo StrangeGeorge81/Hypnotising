452e9c55ab244c8af2b8cf6c785dc0704292714b
StrangeGeorge81 <pandis.g8@gmail.com>
2021-01-05 16:32:52 +0200

    Working Properly, needs adjustments

--- hypno.ino
+++ hypno.ino
@@ -23,34 +23,73 @@ void hypno(){
     //changes hue based on {rep()}
     if (nas==0){
         hue=0;
+        uint32_t rgbcolor = pixels.ColorHSV(hue, 255, brght);
+
         for (nas=0; nas<=5; nas++){
             pixels.setPixelColor (nas, rgbcolor);
             pixels.show();
-            delay(81);
+            delay(250);
         }
     }    
     if (nas==6){
         hue=21845;
+        uint32_t rgbcolor = pixels.ColorHSV(hue, 255, brght);
+
         for(nas=6; nas<=11; nas++){
             pixels.setPixelColor (nas, rgbcolor);
             pixels.show();
-            delay(118);
+            delay(500);
         }
-        if (nas>=11){
-            nas = 0;
-        }
-    }
-    
 
+        if (nas>11){
+            nas = 0;
+            pixels.clear();
+            pixels.show();
+            delay(1000);
+        }
+
+    }
+
+}
+
+void pxlsAct(int tme){
+    pixels.show();
+    delay(tme);
+    pixels.clear();
+    pixels.show();
+}
+
+void flashingR(){
+    for (int i=0; i<=11; i=i+2){
+        hue=0;
+        uint32_t rgbcolor = pixels.ColorHSV(hue, 255, brght);
+        nas=i;
+        pixels.setPixelColor (nas, rgbcolor);
+    }
+    pxlsAct(250);
+    
+}
+
+void flashingG(){
+    for (int i=1; i<=12; i=i+2){
+        hue=21845;
+        uint32_t rgbcolor = pixels.ColorHSV(hue, 255, brght);
+        nas=i;
+        pixels.setPixelColor (nas, rgbcolor);
+    }
+    pxlsAct(500);
+    
 }
 
 void rep(){
     // time depending changes {hypno()}
-    int reption = 0;
-    for(int timer = 0; timer<10; timer++){
-
-        hypno();
-
+    
+    for(int timer = 0; timer<=10; timer++){
+        
+        flashingG();
+        flashingR();
+        nas = 0;
+        
     }
 
 }
@@ -65,5 +104,10 @@ void setup() {
 }
 
 void loop() {
+    hypno();
     rep();
+    pixels.clear();
+    pixels.show();
+    delay (1000);
+
 }
